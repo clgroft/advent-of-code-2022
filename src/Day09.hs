009b51f9c120cae1aab4327352b9cb086f243aa6
Chad Groft <clgroft@gmail.com>
2022-12-09 11:00:50 -0800

    Solve day 9

---
+++ src/Day09.hs
@@ -0,0 +1,71 @@
+-- | Day 9 solution
+module Day09 (day09) where
+
+import Data.List (foldl')
+import Data.Set (Set)
+import qualified Data.Set as S
+
+type Position = (Int, Int)
+
+moveUp :: Position -> Position
+moveUp (r, c) = (r - 1, c)
+
+moveDown :: Position -> Position
+moveDown (r, c) = (r + 1, c)
+
+moveLeft :: Position -> Position
+moveLeft (r, c) = (r, c - 1)
+
+moveRight :: Position -> Position
+moveRight (r, c) = (r, c + 1)
+
+data RopeState = RopeState
+  { rope :: [Position], -- head of list is tail of rope
+    visited :: Set Position
+  }
+
+startState :: Int -> RopeState
+startState n = RopeState (replicate n (0, 0)) $ S.singleton (0, 0)
+
+moveRope :: [Position] -> String -> [Position]
+moveRope [] _ = error "empty rope"
+moveRope [p] "U" = [moveUp p]
+moveRope [p] "D" = [moveDown p]
+moveRope [p] "L" = [moveLeft p]
+moveRope [p] "R" = [moveRight p]
+moveRope [p] dir = error $ "Unrecognized direction: " <> dir
+moveRope (p : ps) dir = reconcile (head ps') p : ps'
+  where
+    ps' = moveRope ps dir
+
+reconcile :: Position -> Position -> Position
+reconcile (r, c) (r', c')
+  | abs (r' - r) <= 1 && abs (c' - c) <= 1 = (r', c')
+  | r' == r && c' == c - 2 = (r', c' + 1)
+  | r' == r && c' == c + 2 = (r', c' - 1)
+  | c' == c && r' == r - 2 = (r' + 1, c')
+  | c' == c && r' == r + 2 = (r' - 1, c')
+  | r' < r && c' < c = (r' + 1, c' + 1)
+  | r' < r && c' > c = (r' + 1, c' - 1)
+  | r' > r && c' < c = (r' - 1, c' + 1)
+  | r' > r && c' > c = (r' - 1, c' - 1)
+  | otherwise = error "illegal rope state"
+
+moveHead :: RopeState -> String -> RopeState
+moveHead (RopeState ps visited) dir = RopeState ps' (S.insert (head ps') visited)
+  where
+    ps' = moveRope ps dir
+
+moveSequence :: [String] -> [String]
+moveSequence = concatMap f
+  where
+    f l = replicate (read w1) w0
+      where
+        [w0, w1] = words l
+
+day09 :: String -> String
+day09 input = unlines [show numVisited, show numVisited2]
+  where
+    moves = moveSequence $ lines input
+    numVisited = S.size . visited $ foldl' moveHead (startState 2) moves
+    numVisited2 = S.size . visited $ foldl' moveHead (startState 10) moves
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -9,6 +9,7 @@ import Day05 (day05)
 import Day06 (day06)
 import Day07 (day07)
 import Day08 (day08)
+import Day09 (day09)
 
 solutions :: Array Int (String -> String)
-solutions = listArray (1, 25) [day01, day02, day03, day04, day05, day06, day07, day08]
+solutions = listArray (1, 25) [day01, day02, day03, day04, day05, day06, day07, day08, day09]
